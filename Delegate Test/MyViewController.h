563c57454e5d29a30114e373a02595fe879a852b
Jordan Hudgens <jordan@wow.com>
2014-01-11 18:49:03 -0600

    Initial Commit

---
+++ Delegate Test/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Delegate Test
+//
+//  Created by Jordan Hudgens on 1/11/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ Delegate Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Delegate Test
+//
+//  Created by Jordan Hudgens on 1/11/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
